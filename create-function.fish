ad64ff7c16e27cf6039362a24bcd543e5f97fd8d
shinyay <shinya.com@gmail.com>
2018-12-26 12:53:38 +0900

    Modify add function on PFS

--- create-function.fish
+++ create-function.fish
@@ -3,7 +3,7 @@
 set -x REGISTRY gcr.io
 set -x REGISTRY_USER (gcloud config get-value core/project)
 
-pfs function create hello \
+pfs function create square \
              --git-repo https://github.com/shinyay/pfs-java-square.git \
              --handler io.pivotal.syanagihara.functions.Square \
              --image $REGISTRY/$REGISTRY_USER/square \
